35a8f86e0f7f102d89a7c8ebf32af22ac9c17521
Ocean Kumar <oceankumar107@gmail.com>
2025-12-08 19:56:23 +0530

    Updated Why Choose Us section and add JS feature

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const links = document.querySelectorAll('a[href^="#"]');
 
 links.forEach(link => {
   link.addEventListener('click', function(e) {
-    e.preventDefault(); // stop the instant jump
+    e.preventDefault(); 
 
     const target = document.querySelector(this.getAttribute('href'));
-    if (!target) return; // safety check
+    if (!target) return; 
 
     target.scrollIntoView({
       behavior: "smooth",
